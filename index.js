8c3c20baab8d1bb5e2afecbba96d9803f37efb77
Tércio Souza de Jesus <terciodejesus@gmail.com>
2025-03-01 12:04:24 -0300

    feat: create timestamp microsservice

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -18,15 +18,22 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:date?", function (req, res) {
+  if (!req.params.date) {
+    const currentTime = new Date()
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+    return res.json({unix: currentTime.getTime(), utc: currentTime.toUTCString()});
+  }
+
+  const date = new Date(isNaN(req.params.date) ? req.params.date : Number(req.params.date));
+
+  if (date == 'Invalid Date') {
+    return res.json({error: 'Invalid Date'})
+  }
+
+  res.json({unix: date.getTime(), utc: date.toUTCString()});
 });
 
-
-
-// Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
